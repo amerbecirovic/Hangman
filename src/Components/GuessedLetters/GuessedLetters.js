9166c4ac7ee4345c34e4372a07f49d334a82af28
Amer Becirovic <amer@Amers-MacBook-Pro.local>
2022-01-08 07:37:18 -0700

    added responsive design for mobile. adjusted some colors.

--- src/Components/GuessedLetters/GuessedLetters.js
+++ src/Components/GuessedLetters/GuessedLetters.js
@@ -31,13 +31,13 @@ export function GuessedLetters(props) {
                 <label>Guess a letter:</label>
                 <input id="guessletter" type="text" maxLength="1" pattern="[a-zA-z]{1}" onChange={handleChangeLetter} required />
                 <br></br>
-                <input type="submit" value="Submit"></input>
+                <input className="submit" type="submit" value="Submit"></input>
             </form>
             <form onSubmit={handleSubmitWord}>
                 <label>Guess a word:</label>
                 <input id="guessword" type="text" minLength="3" maxLength="20" pattern="[a-zA-z]" onChange={handleChangeWord} required />
                 <br></br>
-                <input type="submit" value="Submit"></input>
+                <input className="submit" type="submit" value="Submit"></input>
             </form>
             <h2><u>Guessed Letters</u></h2>
             <h3>{props.guessedLetters.map(elem => elem + ' ')}</h3>
